a8132ff6f354aa106c70a2c445f71e830f3c4e27
sumitdubey <sumitdubey54@gmail.com>
2009-05-09 20:30:10 +0530

    added new features improved file opening closing unsaved changes etc and cut copy paste undo redo although found trivial later on when used RSyntaxTextArea

--- TextEditorDemo.java
+++ TextEditorDemo.java
@@ -3,55 +3,72 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.event.*;
+import javax.swing.JOptionPane;
 import java.io.*;
 import javax.swing.filechooser.*;
 import javax.swing.colorchooser.*;
 import java.awt.image.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
 import javax.imageio.*;
 import java.awt.geom.AffineTransform;
 import java.util.Arrays;
 import org.fife.ui.rtextarea.*;
 import org.fife.ui.rsyntaxtextarea.*;
 
-public class TextEditorDemo extends JFrame implements ActionListener , ItemListener , ChangeListener ,MouseMotionListener,MouseListener
+public class TextEditorDemo extends JFrame implements ActionListener , ItemListener , ChangeListener ,MouseMotionListener,MouseListener ,CaretListener,InputMethodListener,DocumentListener,WindowListener
 {
 
    private static final long serialVersionUID = 1L;
    JFileChooser fcc;                                                   //using filechooser
    String action="";
+   boolean fileChange=false;
+   String title="";
+   InputMethodListener l;
+   File file;
    
    RSyntaxTextArea textArea;
+   Document doc;
 
    public TextEditorDemo() {
 	fcc = new JFileChooser();                                        //For the file opening saving things
 	
       JPanel cp = new JPanel(new BorderLayout());
+      title="Text Editor Demo for Fiji";
 
       textArea = new RSyntaxTextArea();
+      textArea.addInputMethodListener(l);
+      textArea.addCaretListener(this);
       textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+      doc=textArea.getDocument();
+      doc.addDocumentListener(this);
+     // super.fireCaretUpdate(CaretEvent ce);
       RTextScrollPane sp = new RTextScrollPane(textArea);
       cp.add(sp);
 
       	/********* This part is used to change the 
          	   the icon of the 
-           	   window of the editor ***********/
+           	   window of the editor **********/
 
 	BufferedImage image = null;
         try {
-            image = ImageIO.read(cp.getClass().getResource("/images/microscope.gif"));
+           // image = ImageIO.read(cp.getClass().getResource("/images/microscope.gif"));
+	   image = ImageIO.read(new java.net.URL("file:images/icon.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
       setIconImage(image);
 
-
-
+      
+	
             /********setting the icon part ends ********/
 
 
       setContentPane(cp);
-      setTitle("Text Editor Demo for Fiji");
-      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      setTitle(title);
+      addWindowListener(this);
+      setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 
 	/*********** Creating the menu options in the text editor ****************/
 
@@ -91,10 +108,26 @@ public class TextEditorDemo extends JFrame implements ActionListener , ItemListe
         undo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK));
         undo.addActionListener(this);
         edit.addSeparator();
+	JMenuItem redo = new JMenuItem("Redo...");
+        edit.add(redo);
+        redo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, ActionEvent.CTRL_MASK));
+        redo.addActionListener(this);
+        edit.addSeparator();
         JMenuItem cut = new JMenuItem("Cut...");
         edit.add(cut);
         cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
         cut.addActionListener(this);
+        edit.addSeparator();
+	JMenuItem copy = new JMenuItem("Copy...");
+        edit.add(copy);
+        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        copy.addActionListener(this);
+        edit.addSeparator();
+	JMenuItem paste = new JMenuItem("Paste...");
+        edit.add(paste);
+        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
+        paste.addActionListener(this);
+        
         mbar.add(edit);
         
         /******** The Edit menu part ends here *****************/
@@ -109,25 +142,116 @@ public class TextEditorDemo extends JFrame implements ActionListener , ItemListe
 	setVisible(true);
    }
 
+
+
+   		//test if the textArea is changed
+	//on opening change textArea to new Rsyntaxtextarea to clean up the window and check for the flag
+	//open an alert to check if the user wants to save in case the flag is true
+	//creating the buttoned alert and depending on yes , no and cancel
+	//now on closing the window checking if the user wants to save the file (only in case it is changed)
+   
      /******* the function which accounts for the actions in the menu ************/
 	public void actionPerformed(ActionEvent ae){
 		action=ae.getActionCommand();
 		 if (action=="Open...") {
-        		fcc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+			 	fcc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 			int returnVal = fcc.showOpenDialog(TextEditorDemo.this);
 			 try
 			{
-				File file = fcc.getSelectedFile();
+				file = fcc.getSelectedFile();
+			}
+			catch(Exception e){}
+			/*condition to check whether there is a change and the 
+			 * user has really opted to open a new file
+			 */	
+			 if(fileChange==true&&returnVal==fcc.APPROVE_OPTION){
+			//this.setTitle("Not saved - Text Editor Demo for Fiji");
+				int val= JOptionPane.showConfirmDialog(this, "Do you want to save changes??"); 
+				if(val==JOptionPane.YES_OPTION){
+					fileChange=false;
+					saveaction();
+					
+					/*Document listener is removed before opening the file 
+					 * and then added thanx to gitte for telling 
+					 * about DocumentListener
+					 */
+					
+					doc.removeDocumentListener(this);
+					openaction(returnVal);
+					doc.addDocumentListener(this);	
+					
+				}
+				if(val==JOptionPane.NO_OPTION){
+					fileChange=false;
+					doc.removeDocumentListener(this);
+					openaction(returnVal);
+					doc.addDocumentListener(this);
+				}
+				if(val==JOptionPane.CANCEL_OPTION){
+				}	
+			 }	
+					
+			else{
+				doc.removeDocumentListener(this);
+		       		openaction(returnVal);
+				doc.addDocumentListener(this);	
+			}	
+        				
+		}
+		if(action=="Save...") {
+			
+				   fileChange=false;
+				   saveaction();
+		}
+		if(action=="Save as..."){
+			
+				fileChange=false;
+				int temp= saveasaction();                   //temp for the int return type of the function nothing else
+		}		 
+         		     	   
+    		if(action=="Cut..."){
+				textArea.cut();
+		}
+		if(action=="Copy..."){
+				textArea.copy();
+		}
+		if(action=="Paste..."){
+				textArea.paste();
+		}
+		if(action=="Undo..."){
+				textArea.undoLastAction();
+		}
+		if(action=="Redo..."){
+				textArea.redoLastAction();
+		}		
+
+	
+	}
+
+	/*
+	 * this function performs the file opening operation
+	 */
+
+	public void openaction(int returnVal){
+		try{
+			title=(String)file.getName()+" - Text Editor Demo for fiji";
+				this.setTitle(title);
+				
+				/*changing the title part ends*/
+				
 				FileInputStream fin = new FileInputStream(file);
 				//DataInputStream din = new DataInputStream(fin);
 				BufferedReader din = new BufferedReader(new InputStreamReader(fin)); 
 				String s = "";
 				if (returnVal == fcc.APPROVE_OPTION)
 				{
-					while(s != null)
+					textArea.setText("");
+					while(true)
 					{
 						
 						s = din.readLine();
+						if(s==null)
+							break;
 						System.out.println(s);
 						textArea.append(s+"\n");
 						//ba.textArea.setText(s);
@@ -143,26 +267,82 @@ public class TextEditorDemo extends JFrame implements ActionListener , ItemListe
 			catch (Exception ex)
 			{
 			}
-		}
-		if(action=="Save...") {
+	}
+	/*This function performs the saveasoperation i.e. saving in the
+	 * new file
+	 * Basically the motive behind creating different
+	 * functions is because they are used more than one time
+	 * int return type is given just because in one instance of 
+	 * its use the value of the button clicked in the save dialog 
+	 * box was required.
+	 */
+	public int saveasaction(){
+			int returnVal=0;		//just to make sure it does not give a not initialized error
+			boolean ifReplaceFile=false;
 			try{
+				   
          		     	   fcc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-         			   int returnVal = fcc.showDialog(TextEditorDemo.this,"Save");
+         			   returnVal = fcc.showDialog(TextEditorDemo.this,"Save");
          			   if (returnVal == JFileChooser.APPROVE_OPTION) {
-          	   				File fileName = fcc.getSelectedFile();
-						BufferedWriter outFile = new BufferedWriter( new FileWriter( fileName ) );
-						outFile.write( textArea.getText( ) ); //put in textfile
-						outFile.flush( ); // redundant, done by close()
-						outFile.close( );
+          	   				file = fcc.getSelectedFile();
+						String[] filenames;
+         					File f = fcc.getCurrentDirectory();
+          					filenames = f.list();
+          					for(int i=0; i< filenames.length; i++){
+							System.out.println(filenames[i]+" "+file.getName());
+							if(filenames[i].equals(file.getName())){
+								ifReplaceFile=true;
+								break;
+							}	
+						}
+						if(ifReplaceFile){
+								int val= JOptionPane.showConfirmDialog(this, "Do you want to replace "+file.getName()+"??","Do you want to replace "+file.getName()+"??",JOptionPane.YES_NO_OPTION); 
+								if(val==JOptionPane.YES_OPTION){
+								
+									//changing the title again
+					                        	title=(String)file.getName()+" - Text Editor Demo for fiji";
+									this.setTitle(title);
+									BufferedWriter outFile = new BufferedWriter( new FileWriter( file ) );
+									outFile.write( textArea.getText( ) ); //put in textfile
+									outFile.flush( ); // redundant, done by close()
+									outFile.close( );
+								}
+						}
+						else{
+							title=(String)file.getName()+" - Text Editor Demo for fiji";
+							this.setTitle(title);
+							BufferedWriter outFile = new BufferedWriter( new FileWriter( file ) );
+							outFile.write( textArea.getText( ) ); //put in textfile
+							outFile.flush( ); // redundant, done by close()
+							outFile.close( );
+						}
+									
              
               				//This is where a real application would save the file.
             		  }
 			}
 			catch(Exception e){}
-    		}
+			return returnVal;
 
-	
 	}
+        /*It performs save in either new or the same file depending on whether there was a 
+	 * file initially
+	 */
+	public void saveaction(){
+		
+		 if(title=="Text Editor Demo for Fiji"){
+					  int temp= saveasaction();         //temp has no damn use just because saveasaction has a int return type  
+				   }
+				   else {
+					   try{
+						BufferedWriter outFile = new BufferedWriter( new FileWriter( file ) );
+						outFile.write( textArea.getText( ) ); //put in textfile
+						outFile.flush( ); // redundant, done by close()
+						outFile.close( );
+					   }
+				   	catch(Exception e){}	   
+				   }
+	}	
 	
 	public void itemStateChanged(ItemEvent ie){}
 
@@ -176,6 +356,84 @@ public class TextEditorDemo extends JFrame implements ActionListener , ItemListe
 	public void mouseReleased(MouseEvent me){}
 	public void mousePressed(MouseEvent me){}
 
+	//this function is for the caret position change not of much use so far
+	public void caretUpdate(CaretEvent ce){
+	 
+//	fileChange=true;
+	}
+	
+	//next function is for the InputMethodEvent changes
+	public void inputMethodTextChanged(InputMethodEvent event){
+	fileChange=true;
+	}
+	public void caretPositionChanged(InputMethodEvent event){
+	fileChange=true;
+	}
+
+	//Its the real meat the Document Listener functions raise the flag when the text is modifiedd
+	public void insertUpdate(DocumentEvent e){
+		fileChange=true;
+	}
+	public void removeUpdate(DocumentEvent e){
+		fileChange=true;
+	}
+	public void changedUpdate(DocumentEvent e){
+	}
+
+	public void windowActivated(WindowEvent e){
+	}
+	public void windowClosing(WindowEvent e){
+		
+		if(fileChange){
+			int val= JOptionPane.showConfirmDialog(this, "Do you want to save changes??"); 
+				if(val==JOptionPane.YES_OPTION){
+					
+					/*
+					 * This all mess is just to ensure that 
+					 * the window doesnot close on doing foll things stepwise
+					 * step 1)trying to close window with a unsaved file
+					 * step 2)cancelling the save dialog box
+					 */
+					if(title=="Text Editor Demo for Fiji"){
+						int temp=saveasaction();       //temp saves here whether the option was Approved :)
+						if (temp == JFileChooser.APPROVE_OPTION) {
+							fileChange=false;
+							this.dispose();
+						}
+					}
+					else{
+						fileChange=false;
+						saveaction();
+						this.dispose();
+					}	
+						
+					
+				}
+				if(val==JOptionPane.NO_OPTION){
+					fileChange=false;
+					this.dispose();
+				
+				}
+				if(val==JOptionPane.CANCEL_OPTION){
+					setVisible(true);
+				}	
+			 }
+		else {
+			this.dispose();
+		}		
+		
+	}
+	public void windowClosed(WindowEvent e){
+	}
+	public void windowDeactivated(WindowEvent e){
+	}
+	public void windowDeiconified(WindowEvent e){
+	}
+	public void windowIconified(WindowEvent e){
+	}
+	public void windowOpened(WindowEvent e){
+	}	
+		
 
 
    public static void main(String[] args) {
