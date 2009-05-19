7ccd6e1763e17e056d53306eb7e46f6f8b450ad1
sumitdubey <sumitdubey54@gmail.com>
2009-05-19 11:33:32 +0530

    It is the main class which creates the object of the TextEditor class.Still not implemented the ij.plugin.PlugIn interface

---
+++ src-plugins/Script_Editor/fiji/scripting/Editor.java
@@ -0,0 +1,14 @@
+package fiji.scripting;
+import javax.swing.*;
+//import ij.plugin.PlugIn;
+public class Editor{
+	public static void main(String[] arg){
+		//TextEditor editor=new TextEditor();
+		SwingUtilities.invokeLater(new Runnable() {
+         		public void run() {
+            			new TextEditor();
+         		}
+      		});
+	}
+}
+		
