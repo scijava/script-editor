a55665b434c100597484d532af913e29d4c8971f
sumitdubey <sumitdubey54@gmail.com>
2009-05-25 19:23:46 +0530

    I know the name should start from capital could not think of a better name as the one starting with capital is a java interface will certainly change the name next time

---
+++ src-plugins/Script_Editor/fiji/scripting/sortedSet.java
@@ -0,0 +1,11 @@
+package fiji.scripting;
+import java.util.TreeSet;
+public class sortedSet extends TreeSet{
+	public sortedSet(){
+		//this=null;
+	}
+	//public void add(Tree tree)
+	/*public get(int index){
+	}*/
+
+}
