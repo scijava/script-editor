a501ddffdb698279ab586094bdf574cf331dffb3
sumitdubey <sumitdubey54@gmail.com>
2009-06-01 13:09:41 +0530

    this the class which contains the classnames i.e the part ending with .class and it implements Item

---
+++ src-plugins/Script_Editor/fiji/scripting/ClassName.java
@@ -0,0 +1,26 @@
+package fiji.scripting;
+
+import java.lang.Comparable;
+
+public class ClassName implements Item ,Comparable {
+	
+	String key;
+	
+	public ClassName() {
+	}
+
+	public ClassName(String paramkey) {
+		key=paramkey;
+	}	
+	
+	public String getName() {
+		return this.key;
+	}
+	
+	public int compareTo(Object o) {
+		Item tree=(Item)o;
+		return(this.getName().compareTo(tree.getName()));
+	}
+	
+}	
+		
