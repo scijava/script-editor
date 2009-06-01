2103eb0a853c9ceafd50b3204668bd0b144ef1f2
sumitdubey <sumitdubey54@gmail.com>
2009-06-01 13:15:35 +0530

    The class which forms the packages i.e have a String key and is a set of its children which are also of type Item

---
+++ src-plugins/Script_Editor/fiji/scripting/Package.java
@@ -0,0 +1,27 @@
+package fiji.scripting;
+
+import java.util.TreeSet;
+import java.lang.Comparable;
+
+public class Package extends TreeSet<Item> implements Item,Comparable {
+	String key;
+
+	public Package() {
+	}
+
+	public Package(String key) {
+		this.key = key;
+	}
+
+	public String getName() {
+		return this.key;
+	}
+
+	public int compareTo(Object o) {
+		Item tree=(Item)o;
+		return(this.getName().compareTo(tree.getName()));
+	}
+
+
+
+}
