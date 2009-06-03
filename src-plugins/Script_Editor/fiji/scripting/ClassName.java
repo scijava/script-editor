c43f88f46edb3c1a4b83480ec73ff9cb3ceb9a31
sumitdubey <sumitdubey54@gmail.com>
2009-06-03 19:13:16 +0530

    Added a new function getCompleteName which gives the name of just the constructor as the class name with parameters in braces

--- src-plugins/Script_Editor/fiji/scripting/ClassName.java
+++ src-plugins/Script_Editor/fiji/scripting/ClassName.java
@@ -5,16 +5,22 @@ import java.lang.Comparable;
 public class ClassName implements Item ,Comparable {
 
 	String key;
+	String completeName;
 
 	public ClassName() {
 	}
 
-	public ClassName(String paramkey) {
+	public ClassName(String paramkey,String name) {
 		key=paramkey;
+		completeName=name;
 	}
 
 	public String getName() {
 		return this.key;
+	}
+
+	public String getCompleteName() {
+		return this.completeName.replace('/','.');
 	}
 
 	public int compareTo(Object o) {
