b12a7f1c5e2cdcede284d93da4e76d750c0b4c5b
sumitdubey <sumitdubey54@gmail.com>
2009-06-11 09:40:17 +0530

    The class has member variables which are names of the object it represent,the class name and the full class name also implements Comparable. Does the job of giving a robust definition of a object.

---
+++ src-plugins/Script_Editor/fiji/scripting/ImportedClassObjects.java
@@ -0,0 +1,46 @@
+package fiji.scripting;
+
+import java.lang.Comparable;
+
+public class ImportedClassObjects implements Comparable{
+
+	String name;
+	String className;
+	String fullClassName;
+	boolean isImported;
+
+	public ImportedClassObjects(String itsname,String classname,String fullclassname,boolean imported) {
+		name=itsname;
+		className=classname;
+		fullClassName=fullclassname;
+		isImported=imported;
+	}
+
+	public ImportedClassObjects(String itsname,String classname) {
+		name=itsname;
+		className=classname;
+	}
+
+	public ImportedClassObjects(String itsname) {
+		name=itsname;
+	}
+
+	public void setClassName(String classname) {
+		this.className=classname;
+	}
+
+	public void setFullClassName(String fullclassname) {
+		this.fullClassName=fullclassname;
+	}
+
+	public void setIsImported(boolean set) {
+		this.isImported = set;
+	}
+
+	public String getCompleteClassName() {
+		return this.fullClassName;
+	}
+	public int compareTo(Object o) {
+		return(this.name.compareTo(((ImportedClassObjects)o).name));
+	}
+}
