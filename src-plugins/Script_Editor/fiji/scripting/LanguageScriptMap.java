311187d3dc35b6bf280a85a5ddd1c22a6d802584
sumitdubey <sumitdubey54@gmail.com>
2009-06-23 20:28:31 +0530

    I earlier had removed the import statements so I again add the import statements but this is time they are explicit import statements.

--- src-plugins/Script_Editor/fiji/scripting/LanguageScriptMap.java
+++ src-plugins/Script_Editor/fiji/scripting/LanguageScriptMap.java
@@ -3,17 +3,20 @@ package fiji.scripting;
 import java.util.HashMap;
 import java.util.Map;
 import common.*;
+import Clojure.Refresh_Clojure_Scripts;
+import Jython.Refresh_Jython_Scripts;
+import JRuby.Refresh_JRuby_Scripts;
+import Javascript.Refresh_Javascript_Scripts;
+import refreshJavas.Refresh_Javas;
 
 
 public class LanguageScriptMap extends HashMap<String,RefreshScripts>{
 
 	public LanguageScriptMap() {
-		this.put(".clj", new Clojure.Refresh_Clojure_Scripts());
-		this.put(".py", new Jython.Refresh_Jython_Scripts());
-		this.put(".js", new Javascript.Refresh_Javascript_Scripts());
-		this.put(".rb", new JRuby.Refresh_JRuby_Scripts());
-		this.put(".java", new Refresh_Javas.Refresh_Javas());
+		this.put(".clj", new Refresh_Clojure_Scripts());
+		this.put(".py", new Refresh_Jython_Scripts());
+		this.put(".js", new Refresh_Javascript_Scripts());
+		this.put(".rb", new Refresh_JRuby_Scripts());
+		this.put(".java", new Refresh_Javas());
 	}
-
-
 }
