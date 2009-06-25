ca86da175f35bfbac36b2ba2c0c914ed13eca0f6
sumitdubey <sumitdubey54@gmail.com>
2009-06-26 01:48:46 +0530

    The change removes the SwingUtilities.invokeLater() function and also the final keyword in the arguement.

--- src-plugins/Script_Editor/fiji/scripting/Script_Editor.java
+++ src-plugins/Script_Editor/fiji/scripting/Script_Editor.java
@@ -2,26 +2,22 @@ package fiji.scripting;
 
 import javax.swing.*;
 import ij.plugin.PlugIn;
+import ij.Macro;
 
 public class Script_Editor implements PlugIn {
 
 	public static void main(String[] arg){
-		//TextEditor editor=new TextEditor();
-		SwingUtilities.invokeLater(new Runnable() {
-         		public void run() {
-            			new TextEditor("");
-         		}
-      		});
-	}
+		new TextEditor("");
+    }
 
-	public void run(final String arg) {
-		System.out.println(arg);
-		SwingUtilities.invokeLater(new Runnable() {
-         		public void run() {
-            			new TextEditor(arg);
-         		}
-      		});
-	}
+	public void run(String path) {
+		System.out.println(path);
+		String a = Macro.getOptions();
+		System.out.println(Thread.currentThread().getName());
+		try {
+			new TextEditor(Macro.getValue(a,"path",null));
+		} catch(Exception e) { new TextEditor(""); }
+   	}
 
 }
 
