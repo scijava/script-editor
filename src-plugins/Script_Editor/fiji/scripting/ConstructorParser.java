7ebed479a307a396a33b92efa80192af7d59fd2c
sumitdubey <sumitdubey54@gmail.com>
2009-07-01 11:12:50 +0530

    There are two basic changes: i) Now the ConstructorParser uses the most basic statement for each of the language to create the object. ii) The constructor based completion is also provided if the constructor is called with the complete class name like "list =new java.awt.List;" in case of java.

--- src-plugins/Script_Editor/fiji/scripting/ConstructorParser.java
+++ src-plugins/Script_Editor/fiji/scripting/ConstructorParser.java
@@ -10,9 +10,12 @@ public class ConstructorParser {
 	TreeSet<ImportedClassObjects> objectSet=new TreeSet<ImportedClassObjects>();
 	ClassNames names;
 	ArrayList<String> packageNames=new ArrayList<String>();
+	RSyntaxDocument doc;
+	String language;
 
-	public ConstructorParser(ClassNames name) {
+	public ConstructorParser(ClassNames name,String lang) {
 		names=name;
+		language=lang;
 	}
 
 	public TreeSet<ImportedClassObjects> getObjectSet() {
@@ -21,7 +24,7 @@ public class ConstructorParser {
 
 	public void findConstructorObjects(RSyntaxTextArea textArea) {
 
-		RSyntaxDocument doc = (RSyntaxDocument)textArea.getDocument();
+		doc = (RSyntaxDocument)textArea.getDocument();
 		Element map=doc.getDefaultRootElement();
 		int startLine = map.getElementIndex(0);
 		int endLine = map.getElementIndex(doc.getLength());
@@ -32,23 +35,41 @@ public class ConstructorParser {
 			Token prevToPrev=null;
 
 			for(;token!=null&&token.type!=0;token=token.getNextToken()) {
+				System.out.println(token.getLexeme());
 				if(token.type==16||token.type==3||token.type==2||token.type==1) {      //for white space and comments
 					continue;
 				}
-				if(token.getLexeme().equals("new")) {
 
-					if(prev.getLexeme().equals("=")) {
-						if(prevToPrev.type==15) {
-							String temp=getNextNonWhitespaceToken(token).getLexeme();
-							String temp2=isClassPresent(temp,names.root);
-							if(!temp2.equals("")) {
-								ImportedClassObjects obj=new ImportedClassObjects(prevToPrev.getLexeme(),temp,temp2,true);
-								objectSet.add(obj);
-							}
+				if(language.equals("Java")||language.equals("Javascript")||language.equals("Clojure")) {
+					if(token.getLexeme().equals("new")) {
+						setObjectSetIfImported(prevToPrev,prev,getNextNonWhitespaceToken(token));
+						setExplicitObjectSet("JJC",i);
+					}
+				}
+
+				if(language.equals("Python")||language.equals("Matlab")) {
+					if(token.getNextToken().type!=0) {
+						if(token.getNextToken().getLexeme().equals("(")) {
+							setObjectSetIfImported(prevToPrev,prev,token);
+							setExplicitObjectSet("PM",i);
 
 						}
 					}
 				}
+
+				if(language.equals("Ruby")) {
+					if(token.getNextToken().getNextToken().type!=0) {
+						if(token.getNextToken().getNextToken().getLexeme().equals("new")) {
+							setObjectSetIfImported(prevToPrev,prev,token);
+							setExplicitObjectSet("R",i);
+						}
+					}
+					else
+						break;
+				}
+
+
+
 
 				if(!token.isWhitespace()) {
 					prevToPrev=prev;
@@ -61,12 +82,134 @@ public class ConstructorParser {
 		}
 	}
 
+	public void setExplicitObjectSet(String langs,int line) {
+		Token token=doc.getTokenListForLine(line);
+		boolean found=false;
+		Token prev=null;
+		Token prevToPrev=null;
+		String name="";
+		String fullClassName="";
+		for(;token!=null&&token.type!=0;token=token.getNextToken()) {
+			if(token.type==16||token.type==3||token.type==2||token.type==1) {      //for white space and comments
+				continue;
+			}
+
+			if(langs.equals("JJC")) {
+				if(token.getLexeme().equals("new")) {
+					if(prevToPrev.type==15&&prev.getLexeme().equals("=")) {
+						name=prevToPrev.getLexeme();
+						found=true;
+						continue;
+					}
+					else 
+						break;
+
+				}
+
+				if(found) {
+
+					if(token.type==15||token.getLexeme().equals(".")) {
+						fullClassName+=token.getLexeme();
+					}
+					else if(token.getLexeme().equals("(")) {
+						if(!(prev.type==15)) 
+							found=false;
+						break;
+					}
+					else {
+						found=false;
+						break;
+					}
+				}
+			}
+			if(langs.equals("PM")) {
+				if(token.getLexeme().equals("=")) {
+					if(prev.type==15) {
+						name=prev.getLexeme();
+						found=true;
+						continue;
+					}
+					else 
+						break;
+				}
+				if(found) {
+					if(token.type==15||token.getLexeme().equals(".")) {
+						fullClassName+=token.getLexeme();
+					}
+					else if(token.getLexeme().equals("(")) {
+						if(!(prev.type==15)) 
+							found=false;
+						break;
+					}
+					else {
+						found=false;
+						break;
+					}
+				}
+			}
+			if(langs.equals("R")) {
+				if(token.getLexeme().equals("=")) {
+					if(prev.type==15) {
+						name=prev.getLexeme();
+						found=true;
+						continue;
+					}
+					else 
+						break;
+				}
+				if(found) {
+					if(token.getNextToken().type!=0) {
+						if(token.getNextToken().getLexeme().equals("new"))
+							break;
+					}
+
+					if(token.type==15||token.getLexeme().equals(".")) {
+						fullClassName+=token.getLexeme();
+					}
+					else {
+						found=false;
+						break;
+					}
+				}
+			}
+
+			if(!token.isWhitespace()) {
+				prevToPrev=prev;
+				prev=token;
+			}
+		}
+		if(found) {
+			int i=fullClassName.lastIndexOf(".");
+			if(i>0) {
+				ImportedClassObjects obj=new ImportedClassObjects(name,fullClassName.substring(i+1),fullClassName,true);
+				objectSet.add(obj);
+			}
+		}
+	}
+
+
+
+
 	public void setPackageNames(ArrayList<String> names) {
 		packageNames=names;
 	}
 
+	public void setObjectSetIfImported(Token prevToPrev,Token prev,Token classNameToken) {
+		if(prev.getLexeme().equals("=")) {
+			if(prevToPrev.type==15) {
+				String temp=classNameToken.getLexeme();
+				if(classNameToken.getNextToken().getLexeme().equals("(")||classNameToken.getNextToken().getLexeme().equals(")")) {
+					String temp2=isClassPresent(temp,names.root);
+					if(!temp2.equals("")) {
+						ImportedClassObjects obj=new ImportedClassObjects(prevToPrev.getLexeme(),temp,temp2,true);
+						objectSet.add(obj);
+					}
+				}
+			}
+		}
+	}
 	public String isClassPresent(String name,Package root) {
-		System.out.println(packageNames.get(0));
+		//System.out.println(packageNames.get(0));
 		for(String s : packageNames) {
 			String[] parts=s.split("\\.");
 			Item current=findPackage(parts,root);                                    //to create this function
