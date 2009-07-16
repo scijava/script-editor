39e46041b5582c8fa18b550af8fbb754ea480b95
sumitdubey <sumitdubey54@gmail.com>
2009-07-16 09:32:51 +0530

    The test class for the standalone class VMLaunchExample.

--- src-plugins/Script_Editor/Test.java
+++ src-plugins/Script_Editor/Test.java
@@ -2,15 +2,22 @@ import java.util.Random;
 
 public class Test {
 
-  int foo;
+  static int foo;
+  static String name;
 
   public static void main(String[] args) {
-    Random random = new Random();
-    Test test = new Test();
-    for (int i = 0; i < 10; i++) {
-      test.foo = random.nextInt();
-      System.out.println(test.foo);
-    }
+	foo=0;
+    System.out.println("The value of set name is "+name);
+	System.out.println("The value of first arg is "+args[0]);
+	for(int i=0;i<10;i++) {
+		foo+=i;
+	}
+	foo--;
+	foo+=10;
+
   }
 
+	public static void setName(String s){
+		name=s;
+	}
 }
