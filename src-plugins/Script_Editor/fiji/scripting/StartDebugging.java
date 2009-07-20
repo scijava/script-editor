38c22a51a4aa7bb3c4ba69a2b3103c5b6b908bc1
sumitdubey <sumitdubey54@gmail.com>
2009-07-20 11:05:55 +0530

    The changes include : i)Handling of error stream of the launched JVM to print the error. ii) Adding the path to Script_Editor.jar ( but it is the path only for my system) to the default class path.

--- src-plugins/Script_Editor/fiji/scripting/StartDebugging.java
+++ src-plugins/Script_Editor/fiji/scripting/StartDebugging.java
@@ -8,13 +8,15 @@ import com.sun.jdi.request.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.io.IOException;
+import java.io.*;
+
+
 
 
 public class StartDebugging {
 
-	  public String className = "MainClassForDebugging";
-	  public String fieldName;
+	  public String className = "fiji.scripting.MainClassForDebugging";
+	  public String fieldName="foo";
 	  public String plugInName;
 	   List arguments;
 	    int lineNumber;
@@ -31,26 +33,40 @@ public class StartDebugging {
 			lineNumber=line;
 		}
 
-		public void startDebugging() throws InterruptedException,AbsentInformationException,VMStartException{
+		public Process startDebugging() throws IOException,InterruptedException,AbsentInformationException {
+			String s=System.getProperty("java.class.path");
+			String p=File.separator;
+			String s1=System.setProperty("java.class.path",s+File.pathSeparator+"c:"+p+"DOCUME~1"+p+"Sumi"+p+"fiji"+p+"plugins"+p+p+"Script_Editor.jar");
 			VirtualMachine vm = launchVirtualMachine();
-			addClassWatch(vm);
-
-			// resume the vm
+			String s2=System.setProperty("java.class.path",s);
+			System.out.println(s2);
 			vm.resume();
+			addClassWatch(vm);
+			Process process= vm.process();
 
-			// process events
+			InputStream inputStream=process.getErrorStream();
+
 			EventQueue eventQueue = vm.eventQueue();
+
 			while (true) {
 			  EventSet eventSet = eventQueue.remove();
+			  System.out.println("It comes in while");
 			  for (Event event : eventSet) {
+				System.out.println("It comes here");
 				if (event instanceof VMDeathEvent
 					|| event instanceof VMDisconnectEvent) {
+					BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream));
+					System.out.println(reader.readLine());
 				  // exit
-				  return;
+				  //out.flush();
+				  System.out.println(process.exitValue());
+				  return process;
 				} else if (event instanceof ClassPrepareEvent) {
+					System.out.println("It comes in the class prepare event");
 					  // watch field on loaded class
 					ClassPrepareEvent classPrepEvent = (ClassPrepareEvent) event;
 					refType = (ClassType)classPrepEvent.referenceType();
+					System.out.println("The class loaded is "+refType.name());
 					toKnow=refType.fieldByName(fieldName);
 					lineNumber=15;
 					addBreakPointRequest(vm,refType,lineNumber);
@@ -59,12 +75,17 @@ public class StartDebugging {
 					BreakpointEvent breakEvent=(BreakpointEvent)event;
 					System.out.println(refType.getValue(toKnow));
 				}
+				else if(event instanceof VMStartEvent) {
+					System.out.println("The event is VMStartEvent of their type");
+
+				}
 			}
 			eventSet.resume();
-			}
-	  }
+		}
+	}
 
 	private  VirtualMachine launchVirtualMachine() {
+
 		VirtualMachineManager vmm=Bootstrap.virtualMachineManager();
 		LaunchingConnector defConnector=vmm.defaultConnector();
 		Transport transport=defConnector.transport();
@@ -76,22 +97,25 @@ public class StartDebugging {
 		for(String string:s)
 			System.out.println(string);
 		Connector.Argument mainarg=arguments.get("main");
-		mainarg.setValue("MainClassForDebugging "+plugInName);
+		mainarg.setValue("fiji.scripting.MainClassForDebugging plugins/SamplePlugin.java");
 		try {
 			return defConnector.launch(arguments);
+
 		} catch (IOException exc) {
 			throw new Error("Unable to launch target VM: " + exc);
 		} catch (IllegalConnectorArgumentsException exc) {
 			exc.printStackTrace();
-		} /*catch (VMStartException exc) {
+		} catch (VMStartException exc) {
 			throw new Error("Target VM failed to initialize: " +
 			exc.getMessage());
-		}*/
+		} 
+		//System.out.println("Not launched");
 		return null;
 	}
 
   /** Watch all classes of name "Test" */
 	  private  void addClassWatch(VirtualMachine vm) {
+		System.out.println(className);
 		EventRequestManager erm = vm.eventRequestManager();
 		ClassPrepareRequest classPrepareRequest = erm
 			.createClassPrepareRequest();
