527f069f98d252e0ca8df387c808b9bb9e029b75
sumitdubey <sumitdubey54@gmail.com>
2009-07-20 11:09:08 +0530

    Made the main class very simple to realize the value of the breakpoint and the value of foo at the breakpoint.

--- src-plugins/Script_Editor/fiji/scripting/MainClassForDebugging.java
+++ src-plugins/Script_Editor/fiji/scripting/MainClassForDebugging.java
@@ -1,19 +1,29 @@
 package fiji.scripting;
-
-import ij.*;
+//import ij.*;
 import java.io.File;
 
 public class MainClassForDebugging {
 
+	static int foo;
+
 	public static void main(String args[]) {
-		if(IJ.getInstance()!=null) {
+		/*if(IJ.getInstance()!=null) {
 			new ImageJ();
-		}
-		String className=findClassName(args[0]);
-		Object obj=IJ.runPlugIn(className,"");
+		}*/
+
+		foo++;
+		foo--;
+		foo+=2;
+		foo+=3;
+		foo--;
+		String s="The value of foo is "+foo;
+		System.err.write(s.getBytes(),0,s.getBytes().length);
+		//IJ.error("the main function");
+		/*String className=findClassName(args[0]);
+		Object obj=IJ.runPlugIn(className,"");*/
 	}
 
-	public static String findClassName(String path) {
+	/*public static String findClassName(String path) {
 		String c = path;
         if (c.endsWith(".java")) {
 			 c = c.substring(0, c.length() - 5);
@@ -27,7 +37,7 @@ public class MainClassForDebugging {
 				c = c.substring(1);
 		}
 		return(c.replace('/','.'));
-	}
+	}*/
 
 }
 
