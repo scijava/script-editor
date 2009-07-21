5ddf6a63009cd71608762b032621b19cc243a99d
sumitdubey <sumitdubey54@gmail.com>
2009-07-21 13:53:39 +0530

    This is the main class now in the fiji/stub folder. Still giving some error as the ij package is unavailable(technically NoClassDefFoundError).

---
+++ stub/MainClassForDebugging.java
@@ -0,0 +1,42 @@
+package stub;
+import ij.*;
+import java.io.File;
+
+public class MainClassForDebugging {
+
+	static int foo;
+
+	public static void main(String args[]) {
+		if(IJ.getInstance()!=null) {
+			new ImageJ();
+		}
+
+		foo++;
+		foo--;
+		foo+=2;
+		foo+=3;
+		foo--;
+		String s="The argument is "+args[0];
+		System.err.write(s.getBytes(),0,s.getBytes().length);
+		String className=findClassName(args[0]);
+		Object obj=IJ.runPlugIn(className,"");
+	}
+
+	public static String findClassName(String path) {
+		String c = path;
+        if (c.endsWith(".java")) {
+			 c = c.substring(0, c.length() - 5);
+		}
+		String pluginsPath = Menus.getPlugInsPath();
+		if (!pluginsPath.endsWith(File.separator))
+			pluginsPath += File.separator;
+		if (c.startsWith(pluginsPath)) {
+			c = c.substring(pluginsPath.length());
+			while (c.startsWith(File.separator))
+				c = c.substring(1);
+		}
+		return(c.replace('/','.'));
+	}
+
+}
+
