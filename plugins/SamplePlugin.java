1d947f2b919ca80d7d4a1587184d60790fe91a58
sumitdubey <sumitdubey54@gmail.com>
2009-07-23 23:02:24 +0530

    Making foo as the class member. I constantly get a warning "LF will be replaced by CRLF in plugins/SamplePlugin.java" every time I commit this file.

--- plugins/SamplePlugin.java
+++ plugins/SamplePlugin.java
@@ -3,12 +3,13 @@ import ij.*;
 import java.awt.Frame;
 
 public class SamplePlugin implements PlugIn {
+	static int foo;
 	public static void main(String args[]){
 		System.out.println("This is a plugin");
 	}
 	public void run(String path) {
 		System.out.println("It is running");
-		static int foo=0;
+		foo=0;
 		foo++;
 		foo+=2;
 		foo--;
