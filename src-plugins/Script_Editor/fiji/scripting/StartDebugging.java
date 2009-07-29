33897dc2cf84dbd4cbd5d4ab63a286cbb225f855
sumitdubey <sumitdubey54@gmail.com>
2009-07-29 18:37:33 +0530

    Change to explicit import statements and read from InputStream using new Thread.The className to look for now is any java file in the editor window.

--- src-plugins/Script_Editor/fiji/scripting/StartDebugging.java
+++ src-plugins/Script_Editor/fiji/scripting/StartDebugging.java
@@ -1,19 +1,41 @@
 package fiji.scripting;
 
-import com.sun.jdi.*;
-import com.sun.jdi.connect.*;
-import com.sun.jdi.connect.spi.*;
-import com.sun.jdi.event.*;
-import com.sun.jdi.request.*;
+import com.sun.jdi.Location;
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.VirtualMachineManager;
+import com.sun.jdi.Bootstrap;
+import com.sun.jdi.ClassType;
+import com.sun.jdi.ReferenceType;
+import com.sun.jdi.AbsentInformationException;
+import com.sun.jdi.Field;
+import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.Transport;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.connect.VMStartException;
+import com.sun.jdi.event.Event;
+import com.sun.jdi.event.EventSet;
+import com.sun.jdi.event.EventQueue;
+import com.sun.jdi.event.BreakpointEvent;
+import com.sun.jdi.event.VMStartEvent;
+import com.sun.jdi.event.VMDeathEvent;
+import com.sun.jdi.event.ClassPrepareEvent;
+import com.sun.jdi.event.VMDisconnectEvent;
+import com.sun.jdi.request.ClassPrepareRequest;
+import com.sun.jdi.request.BreakpointRequest;
+import com.sun.jdi.request.EventRequestManager;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Iterator;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import ij.IJ;
 
 public class StartDebugging {
 
-	  public String className = "SamplePlugin";
+	  public String className;
 	  public String fieldName="foo";
 	  public String plugInName;
 	   List arguments;
@@ -37,18 +59,33 @@ public class StartDebugging {
 
 			vm = launchVirtualMachine();
 			vm.resume();
+			if(plugInName.endsWith(".java")) {
+				String fileName=plugInName.substring(plugInName.lastIndexOf(File.separator)+1);
+				className=fileName.substring(0,fileName.length()-5);
+			}
 			addClassWatch(vm);
 			Process process= vm.process();
-			InputStream inputStream=process.getErrorStream();
+			final InputStream inputStream=process.getErrorStream();
+			new Thread() {
+				public void run() {
+					byte[] buffer = new byte[16384];
+					for (;;) {
+						try {
+							int count = inputStream.read(buffer);
+							if (count < 0)
+								return; // EOF
+							System.out.println(new String(buffer, 0, count));
+						}catch(IOException e){}
+					}
+				}
+			}.start();
 			EventQueue eventQueue = vm.eventQueue();
 
 			while (true) {
 			  EventSet eventSet = eventQueue.remove();
 			  for (Event event : eventSet) {
 				if (event instanceof VMDeathEvent || event instanceof VMDisconnectEvent) {
-					BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream));
-					System.out.println(reader.readLine());
-				    return process;
+					return process;
 				} else if (event instanceof ClassPrepareEvent) {
 					System.out.println("It comes in the class prepare event");
 					  // watch field on loaded class
@@ -99,7 +136,7 @@ public class StartDebugging {
 			System.out.println(string);
 		Connector.Argument mainarg=arguments.get("main");
 		String s1=System.getProperty("java.class.path");
-		mainarg.setValue("-classpath \""+s1+"\" fiji.MainClassForDebugging "+plugInName.substring(plugInName.lastIndexOf(File.separator)+1));
+		mainarg.setValue("-classpath \""+s1+"\" fiji.MainClassForDebugging "+plugInName);
 
 		try {
 			return defConnector.launch(arguments);
