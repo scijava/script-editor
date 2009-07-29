0ac91cc2272b15f06a265bcfa0bfeb17d64069c2
sumitdubey <sumitdubey54@gmail.com>
2009-07-29 18:47:38 +0530

    Change to explicit import statements and removed the unnecessary this operator.

--- src-plugins/Script_Editor/fiji/scripting/LanguageScriptMap.java
+++ src-plugins/Script_Editor/fiji/scripting/LanguageScriptMap.java
@@ -1,21 +1,20 @@
 package fiji.scripting;
 
 import java.util.HashMap;
-import java.util.Map;
-import common.*;
+import common.RefreshScripts;
 import Clojure.Refresh_Clojure_Scripts;
 import Jython.Refresh_Jython_Scripts;
 import JRuby.Refresh_JRuby_Scripts;
 import Javascript.Refresh_Javascript_Scripts;
-import refreshJavas.Refresh_Javas;
+import fiji.scripting.java.Refresh_Javas;
 
 public class LanguageScriptMap extends HashMap<String,RefreshScripts>{
 
 	public LanguageScriptMap() {
-		this.put(".clj", new Refresh_Clojure_Scripts());
-		this.put(".py", new Refresh_Jython_Scripts());
-		this.put(".js", new Refresh_Javascript_Scripts());
-		this.put(".rb", new Refresh_JRuby_Scripts());
-		this.put(".java", new Refresh_Javas());
+		put(".clj", new Refresh_Clojure_Scripts());
+		put(".py", new Refresh_Jython_Scripts());
+		put(".js", new Refresh_Javascript_Scripts());
+		put(".rb", new Refresh_JRuby_Scripts());
+		put(".java", new Refresh_Javas());
 	}
 }
