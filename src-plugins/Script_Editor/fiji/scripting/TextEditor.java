0d6dccc54e7cb6c9c2412820fea68732f73b4196
sumitdubey <sumitdubey54@gmail.com>
2009-07-29 17:59:42 +0530

    Refactor the code.Apply only one class for "Find" and "Replace" dialogs.Remove some flaws in opening and creating a "new" file. Moved the completion stuff.

--- src-plugins/Script_Editor/fiji/scripting/TextEditor.java
+++ src-plugins/Script_Editor/fiji/scripting/TextEditor.java
@@ -1,29 +1,71 @@
 package fiji.scripting;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.event.*;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JPanel;
+import javax.swing.JFileChooser;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JMenuBar;
+import javax.swing.BorderFactory;
+import javax.swing.KeyStroke;
+import javax.swing.ToolTipManager;
 import javax.swing.JOptionPane;
-import java.io.*;
-import javax.swing.filechooser.*;
-import javax.swing.colorchooser.*;
-import java.awt.image.*;
-import javax.swing.event.DocumentEvent;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.BorderLayout;
+import java.awt.event.WindowEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseListener;
+import java.awt.event.InputMethodListener;
+import java.awt.event.WindowListener;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.CaretEvent;
 import javax.swing.text.Document;
-import ij.io.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import ij.io.OpenDialog;
+import ij.io.SaveDialog;
 import ij.IJ;
 import ij.Prefs;
-import javax.imageio.*;
-import java.util.Arrays;
-import java.util.List;
-import javax.swing.text.*;
-import org.fife.ui.rtextarea.*;
-import org.fife.ui.rsyntaxtextarea.*;
-import org.fife.ui.autocomplete.*;
+import javax.imageio.ImageIO;
+import org.fife.ui.rtextarea.Gutter;
+import org.fife.ui.rtextarea.RTextScrollPane;
+import org.fife.ui.rtextarea.ToolTipSupplier;
+import org.fife.ui.rtextarea.IconGroup;
+import org.fife.ui.rsyntaxtextarea.RSyntaxDocument;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.autocomplete.AutoCompletion;
+import org.fife.ui.autocomplete.BasicCompletion;
+import org.fife.ui.autocomplete.CompletionProvider;
+import org.fife.ui.autocomplete.DefaultCompletionProvider;
 import common.RefreshScripts;
 import com.sun.jdi.connect.VMStartException;
+
+import fiji.scripting.completion.ClassCompletionProvider;
+import fiji.scripting.completion.DefaultProvider;
 
 
 class TextEditor extends JFrame implements ActionListener , ItemListener , ChangeListener ,MouseMotionListener,MouseListener ,CaretListener,InputMethodListener,DocumentListener,WindowListener	{
@@ -42,8 +84,7 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 	JMenuItem new1,open,save,saveas,compileAndRun,debug,quit,undo,redo,cut,copy,paste,find,replace,selectAll,autocomplete,jfcdialog,ijdialog,resume,terminate;
 	JRadioButtonMenuItem[] lang=new JRadioButtonMenuItem[7];
 	FileInputStream fin;
-      	FindDialog findDialog;
-   	ReplaceDialog replaceDialog;
+    FindAndReplaceDialog replaceDialog;
 	AutoCompletion autocomp;
 	LanguageScriptMap scriptmap=new LanguageScriptMap();
 	ClassCompletionProvider provider;
@@ -266,37 +307,31 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 	//now on closing the window checking if the user wants to save the file (only in case it is changed)
    
      /******* the function which accounts for the actions in the menu ************/
+	 
+	public void createNewDocument() {
+		doc.removeDocumentListener(this);
+		textArea.setText("");
+		this.setTitle("TextEditor for Fiji");
+		isFileUnnamed=true;
+		doc.addDocumentListener(this);
+	}
+
 	public void actionPerformed(ActionEvent ae){
 		if(ae.getSource()==new1){
 			if(fileChange==true) {
-				int val= JOptionPane.showConfirmDialog(this, "Do you want to save changes??"); 
-				if(val==JOptionPane.YES_OPTION){
-					fileChange=false;
-					saveaction();
-					doc.removeDocumentListener(this);
-					textArea.setText("");
-				    this.setTitle("Text Editor for Fiji");
-					isFileUnnamed=true;
-					doc.addDocumentListener(this);
-
-				}
-				if(val==JOptionPane.NO_OPTION) {
-					fileChange=false;
-					doc.removeDocumentListener(this);
-					textArea.setText("");
-					this.setTitle("TextEditor for Fiji");
-					doc.addDocumentListener(this);
-				}
+				int val= JOptionPane.showConfirmDialog(this, "Do you want to save changes??");
 				if(val==JOptionPane.CANCEL_OPTION) {
+					return;
+				}
+				else if(val==JOptionPane.YES_OPTION){
+					if(save()!=JFileChooser.APPROVE_OPTION)
+						return;
+				}
+				else if(val!=JOptionPane.NO_OPTION) {
+					return;
 				}
 			 }
-
-			else{
-				doc.removeDocumentListener(this);
-		       	textArea.setText("");
-				this.setTitle("TextEditor for Fiji");
-				doc.addDocumentListener(this);
-			}
+			createNewDocument();
 		}
 
 		if (ae.getSource()==open) {
@@ -321,34 +356,28 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 				/*condition to check whether there is a change and the 
 			 	* user has really opted to open a new file
 			 	*/
-			if(fileChange==true&&returnVal==fcc.APPROVE_OPTION) {
-				int val= JOptionPane.showConfirmDialog(this, "Do you want to save changes??"); 
-				if(val==JOptionPane.YES_OPTION){
-					fileChange=false;
-					saveaction();
-					open(path);
+			if(returnVal==fcc.APPROVE_OPTION) {
+				if(fileChange==true) {
+					int val= JOptionPane.showConfirmDialog(this, "Do you want to save changes???"); 
+					if(val==JOptionPane.CANCEL_OPTION){
+						return;
+					}
+					else if(val==JOptionPane.YES_OPTION){
+						if(save()!=JFileChooser.APPROVE_OPTION)
+							return;
+					}
+					else if(val!=JOptionPane.NO_OPTION){
+						return;
+					} 
 				}
-				if(val==JOptionPane.NO_OPTION){
-					fileChange=false;
-					open(path);
-				}
-				if(val==JOptionPane.CANCEL_OPTION){
-				}
-			 }
-
-			if(fileChange==false&&returnVal==fcc.APPROVE_OPTION) {
-		       		open(path);
+				open(path);
 			}
         
 		}
 		if(ae.getSource()==save) {
-
-			fileChange=false;
-			saveaction();
+			int temp=save();
 		}
 		if(ae.getSource()==saveas)  {
-
-			fileChange=false;
 			int temp= saveasaction();                   //temp for the int return type of the function nothing else
 		}
 		if(ae.getSource()==compileAndRun) {
@@ -357,13 +386,11 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 		if(ae.getSource()==debug) {
 			BreakpointManager manager=new BreakpointManager(gutter,textArea,iconGroup);
 			debugging=new StartDebugging(file.getPath(),manager.findBreakpointsLineNumber());
+
 			try {
 				System.out.println(debugging.startDebugging().exitValue());
 			} 
 			catch(Exception e){}
-
-
-
 		}
 		if(ae.getSource()==quit) {
 			processWindowEvent( new WindowEvent(this, WindowEvent.WINDOW_CLOSING) );
@@ -385,31 +412,13 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 			textArea.redoLastAction();
 		}
 		if(ae.getSource()==find) {
-			if(replaceDialog!=null){						//here should the code to close all other dialog boxes 
-				replaceDialog.dispose();
-			}
-			if(findDialog==null){							//checking if there is no other find dialogue boxes open
-				findDialog=new FindDialog(this,textArea);
-				findDialog.setResizable(true);
-				findDialog.pack();
-				findDialog.setLocationRelativeTo(this);
-			}
-			findDialog.show();
-			findDialog.toFront();
+
+			setFindAndReplace(false);
 		}
 		if(ae.getSource()==replace) {						//here should the code to close all other dialog boxes
 			try{
-				if(findDialog!=null){
-					findDialog.dispose();
-				}
-				if(replaceDialog==null){
-					replaceDialog=new ReplaceDialog(this,textArea);
-					replaceDialog.setResizable(true);
-					replaceDialog.pack();
-					replaceDialog.setLocationRelativeTo(this);
-				}
-				replaceDialog.show();
-				replaceDialog.toFront();
+				setFindAndReplace(true);
+
 			} catch(Exception e){ e.printStackTrace(); }
 		}
 
@@ -476,10 +485,25 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 	 * this function performs the file opening operation
 	 */
 
+	public void setFindAndReplace(boolean ifReplace) {
+		if(replaceDialog!=null){						//here should the code to close all other dialog boxes 
+			if(replaceDialog.isReplace()!=ifReplace) {
+				replaceDialog.dispose();
+				replaceDialog=null;
+			}
+		}
+		if(replaceDialog==null){
+			replaceDialog=new FindAndReplaceDialog(this,textArea,ifReplace);
+			replaceDialog.setResizable(true);
+			replaceDialog.pack();
+			replaceDialog.setLocationRelativeTo(this);
+		}
+		replaceDialog.show();
+		replaceDialog.toFront();
+	}
 
 	public void open(String path) {
 
-		System.out.println("It is opened");
 		try {
 				file = new File(path);
 		}
@@ -487,23 +511,26 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 		doc.removeDocumentListener(this);
 		try {
 			if(file!=null) {
+				fileChange=false;
 				setLanguage(file);
 				isFileUnnamed=false;
-			}
 				/*changing the title part ends*/
-			fin = new FileInputStream(file);
-			BufferedReader din = new BufferedReader(new InputStreamReader(fin)); 
-			String s = "";
+				fin = new FileInputStream(file);
+				BufferedReader din = new BufferedReader(new InputStreamReader(fin)); 
+				String s = "";
 
-			textArea.setText("");
-			while(true) {
-				s = din.readLine();
-				if(s==null) {
-					break;
+				textArea.setText("");
+				while(true) {
+					s = din.readLine();
+					if(s==null) {
+						break;
+					}
+					textArea.append(s+"\n");
 				}
-				textArea.append(s+"\n");
 			}
-
+			else {
+				JOptionPane.showMessageDialog(this,"The file name "+file.getName()+" not found.");
+			}
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -528,88 +555,94 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 	 * box was required.
 	 */
 	public int saveasaction() {
-			int returnVal=0;		//just to make sure it does not give a not initialized error
-			boolean ifReplaceFile=false;
-			String path="";
-			String directory="";
+		SaveDialog sd = new SaveDialog("Save as ","new",".java");
+		String name = sd.getFileName();
+		if(name!=null) {
+			String path=sd.getDirectory()+name;
+			return(saveAs(path,checkForReplace(sd.getDirectory(),name)));
 
-			try{
+		}
+		else {
+			return -1;
+		}
+	}
 
-					SaveDialog sd = new SaveDialog("Save as ","new",".java");
-					String name = sd.getFileName();
-					if(name!=null) {
-						directory = sd.getDirectory();
-						path = directory+name;
-						returnVal=JFileChooser.APPROVE_OPTION;
-					}
-
-         			   if (returnVal == JFileChooser.APPROVE_OPTION) {
-
-							if(!Prefs.useJFileChooser) {
-								file = new File(path);
-								f = new File(directory);
-							}
-							else {
-								file = fcc.getSelectedFile();
-	         					f = fcc.getCurrentDirectory();
-							}
-							String[] filenames;
-          					filenames = f.list();
-          					for(int i=0; i< filenames.length; i++) {
-									if(filenames[i].equals(file.getName())) {
-									ifReplaceFile=true;
-									break;
-								}
-							}
-							if(ifReplaceFile) {
-									int val= JOptionPane.showConfirmDialog(this, "Do you want to replace "+file.getName()+"??","Do you want to replace "+file.getName()+"??",JOptionPane.YES_NO_OPTION); 
-									if(val==JOptionPane.YES_OPTION) {
-
-										//changing the title again
-													title=(String)file.getName()+" - Text Editor Demo for fiji";
-										this.setTitle(title);
-										BufferedWriter outFile = new BufferedWriter( new FileWriter( file ) );
-										outFile.write( textArea.getText( ) ); //put in textfile
-										outFile.flush( ); // redundant, done by close()
-										outFile.close( );
-									}
-							}
-							else {
-								setLanguage(file);
-								BufferedWriter outFile = new BufferedWriter( new FileWriter( file ) );
-								outFile.write( textArea.getText( ) ); //put in textfile
-								outFile.flush( ); // redundant, done by close()
-								outFile.close( );
-							}
-
-             
-              				//This is where a real application would save the file.
-            		  }
+	public boolean checkForReplace(String directory,String name) {
+		boolean ifReplaceFile=false;
+		if(!Prefs.useJFileChooser) {
+			f = new File(directory);
+		}
+		else {
+   			f = fcc.getCurrentDirectory();
+		}
+		String[] filenames;
+		filenames = f.list();
+		for(int i=0; i< filenames.length; i++) {
+			if(filenames[i].equals(name)) {
+				ifReplaceFile=true;
+				break;
 			}
-			catch(Exception e){
+		}
+		return ifReplaceFile;
+	}
+
+	public void saveAs(String path) {
+		int temp=saveAs(path,true);
+	}
+
+	public int saveAs(String path,boolean ifReplaceFile) {
+		if(!Prefs.useJFileChooser) {
+			file=new File(path);
+		}
+		else {
+			file=fcc.getSelectedFile();
+		}
+		try{
+			if(ifReplaceFile) {
+				int val= JOptionPane.showConfirmDialog(this, "Do you want to replace "+file.getName()+"??","Do you want to replace "+file.getName()+"??",JOptionPane.YES_NO_OPTION); 
+				if(val==JOptionPane.YES_OPTION) {
+					title=(String)file.getName()+" - Text Editor Demo for fiji";
+					this.setTitle(title);
+					writeToFile(file);
+				}
+				else 
+					return -1;
+			}
+			else {
+				setLanguage(file);
+				writeToFile(file);
+			}          
+        
+		}catch(Exception e){
 				e.printStackTrace();
-			}
-			return returnVal;
-
+		}
+		return JFileChooser.APPROVE_OPTION;
 	}
         /*It performs save in either new or the same file depending on whether there was a 
 	 * file initially
 	 */
-	public void saveaction(){
+	public int save(){
 
 		if(isFileUnnamed) {
-			int temp= saveasaction();         //temp has no use just because saveasaction has a int return type  
+			return(saveasaction());         //temp has no use just because saveasaction has a int return type  
 		}
 		else {
-			try {
-				BufferedWriter outFile = new BufferedWriter( new FileWriter( file ) );
-				outFile.write( textArea.getText( ) ); //put in textfile
-				outFile.flush( ); // redundant, done by close()
-				outFile.close( );
-			}
-			catch(Exception e){}	   
+			writeToFile(file);
+			return JFileChooser.APPROVE_OPTION;
 		}
 	}
+
+	public void writeToFile(File file) {
+		try {
+			fileChange=false;
+			BufferedWriter outFile = new BufferedWriter( new FileWriter( file ) );
+			outFile.write( textArea.getText( ) ); //put in textfile
+			outFile.flush( ); // redundant, done by close()
+			outFile.close( );
+		}
+		catch(Exception e){}
+	}
+
 
 	public void itemStateChanged(ItemEvent ie) {}
 	public void stateChanged(ChangeEvent e) {}
@@ -682,26 +715,10 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 		if(fileChange||isFileUnnamed) {
 			int val= JOptionPane.showConfirmDialog(this, "You must save the changes before running.Do you want to save changes??","Select an Option",JOptionPane.YES_NO_OPTION);
 			if(val==JOptionPane.YES_OPTION){
-
-				if(isFileUnnamed) {
-					int temp=saveasaction();       //temp saves here whether the option was Approved :)
+					int temp=save();       //temp saves here whether the option was Approved :)
 					if (temp == JFileChooser.APPROVE_OPTION) {
-						fileChange=false;
 						certainScriptRun();
 					}
-					else {
-						JOptionPane.showMessageDialog(this, "The script could not run because you did not save the file.");
-					}
-				}
-				else {
-					fileChange=false;
-					saveaction();
-					certainScriptRun();
-				}
-			}
-
-			if(val==JOptionPane.NO_OPTION) {
-				JOptionPane.showMessageDialog(this, "The script could not run because you did not save the file.");
 			}
 		}
 		else {
@@ -727,35 +744,17 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 
 		if(fileChange) {
 			int val= JOptionPane.showConfirmDialog(this, "Do you want to save changes??"); 
+			if(val==JOptionPane.CANCEL_OPTION) {
+				setVisible(true);
+				return;
+			}
 			if(val==JOptionPane.YES_OPTION){
-
-				if(isFileUnnamed) {
-					int temp=saveasaction();       //temp saves here whether the option was Approved :)
-					if (temp == JFileChooser.APPROVE_OPTION) {
-						fileChange=false;
-						this.dispose();
-					}
+				if (save() != JFileChooser.APPROVE_OPTION) {
+					return;
 				}
-				else {
-					fileChange=false;
-					saveaction();
-					this.dispose();
-				}
-
-
-				}
-				if(val==JOptionPane.NO_OPTION) {
-					fileChange=false;
-					this.dispose();
-
-				}
-				if(val==JOptionPane.CANCEL_OPTION) {
-					setVisible(true);
-				}
-			 }
-		else {
-			this.dispose();
+			}
 		}
+		this.dispose();
 
 	}
 	public void windowClosed(WindowEvent e) {}
@@ -767,13 +766,6 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 /*autocomplete addition starts here*/
 
 
-	/**
-	 * Returns the provider to use when in a comment.
-	 *
-	 * @return The provider.
-	 * @see #createCodeCompletionProvider()
-	 * @see #createStringCompletionProvider()
-	 */
 	private CompletionProvider createCommentCompletionProvider() {
 		DefaultCompletionProvider cp = new DefaultCompletionProvider();
 		cp.addCompletion(new BasicCompletion(cp, "TODO:", "A to-do reminder"));
@@ -805,13 +797,6 @@ class TextEditor extends JFrame implements ActionListener , ItemListener , Chang
 
 
 
-	/**
-	 * Returns the completion provider to use when the caret is in a string.
-	 *
-	 * @return The provider.
-	 * @see #createCodeCompletionProvider()
-	 * @see #createCommentCompletionProvider()
-	 */
 	private CompletionProvider createStringCompletionProvider() {
 
 		DefaultCompletionProvider cp = new DefaultCompletionProvider();
