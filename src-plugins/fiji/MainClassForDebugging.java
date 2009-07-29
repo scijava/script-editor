f27a05e2da734a8818478722b3e9c9182288d21d
sumitdubey <sumitdubey54@gmail.com>
2009-07-29 18:52:11 +0530

    Remove some unnecessary code and change definition of findClassName ,now it compares file using equals function of java.io.File.

--- src-plugins/fiji/MainClassForDebugging.java
+++ src-plugins/fiji/MainClassForDebugging.java
@@ -1,6 +1,8 @@
 package fiji;
 
-import ij.*;
+import ij.IJ;
+import ij.Menus;
+import ij.ImageJ;
 import java.io.File;
 
 public class MainClassForDebugging {
@@ -12,9 +14,16 @@ public class MainClassForDebugging {
 		if(IJ.getInstance()==null) {
 			new ImageJ();
 		}
+		String path="";
+		int i=0;
+		while(i<args.length-1) {
 
-		String s=System.getProperty("java.class.path");
-		className=findClassName(args[0]);
+			path+=args[i];
+			i++;
+			path+=" ";
+		}
+		path+=args[i];
+		className=findClassName(path);
 		try {
 			IJ.runPlugIn(className,"");
 		} catch(Exception e) { e.printStackTrace(); }
@@ -24,19 +33,29 @@ public class MainClassForDebugging {
 
 	public static String findClassName(String path) {
 		String c = path;
+		String c1= path;
         if (c.endsWith(".java")) {
-			 c = c.substring(0, c.length() - 5);
+			c = c.substring(0, c.length() - 5);
 		}
 		String pluginsPath = Menus.getPlugInsPath();
+		File f1=new File(pluginsPath);
 		if (!pluginsPath.endsWith(File.separator))
 			pluginsPath += File.separator;
-		if (c.startsWith(pluginsPath)) {
-			c = c.substring(pluginsPath.length());
-			while (c.startsWith(File.separator))
-				c = c.substring(1);
+		boolean check=false;
+		while(c1.lastIndexOf(File.separator)>0) {
+			File f2=new File(c1);
+			if(f2.compareTo(f1)==0) {
+				check=true;
+				break;
+			}else
+				c1=c1.substring(0,c1.lastIndexOf(File.separator));
 		}
-
-			return(c.replace('/','.'));
+		if(check) {
+			c=c.substring(c1.length());
+			while(c.startsWith(File.separator))
+				c=c.substring(1);
+		}
+		return(c.replace('/','.'));
 	}
 
 }
