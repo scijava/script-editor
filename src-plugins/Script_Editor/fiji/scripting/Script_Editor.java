a8a8f6ddc8e999f78eaaa26340038f6acc02415d
sumitdubey <sumitdubey54@gmail.com>
2009-07-29 18:50:03 +0530

    Remove unnecessary code.

--- src-plugins/Script_Editor/fiji/scripting/Script_Editor.java
+++ src-plugins/Script_Editor/fiji/scripting/Script_Editor.java
@@ -1,14 +1,9 @@
 package fiji.scripting;
 
-import javax.swing.*;
 import ij.plugin.PlugIn;
 import ij.Macro;
 
 public class Script_Editor implements PlugIn {
-
-	public static void main(String[] arg){
-		new TextEditor("");
-    }
 
 	public void run(String path) {
 		String a = Macro.getOptions();
