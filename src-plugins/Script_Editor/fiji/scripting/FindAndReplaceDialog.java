2d7a04d8ff0fb8332a32838bf9eac02e3e41dadb
sumitdubey <sumitdubey54@gmail.com>
2009-07-31 23:37:42 +0530

    Refactor the code.Introduce several new functions to avoid repetetion of code.Remove the FindDocumentListener,FindFocusAdapter,FindKeyListener classes.

--- src-plugins/Script_Editor/fiji/scripting/FindAndReplaceDialog.java
+++ src-plugins/Script_Editor/fiji/scripting/FindAndReplaceDialog.java
@@ -34,66 +34,38 @@ public class FindAndReplaceDialog extends JDialog implements ActionListener {
 	JCheckBox regexCB;
 	JTextField searchField;
 	JCheckBox matchCaseCB;
-	JCheckBox markAllCheckBox;
-	JCheckBox wholeWordCheckBox;
+	JCheckBox markAllCB;
+	JCheckBox wholeWordCB;
 	JTextField replaceField;
 	boolean ifReplace;
+	boolean forward=true;
+	boolean matchCase,wholeWord,markAll,regex;
+	ComponentOrientation orientation;
 	RSyntaxTextArea textArea;
+	JPanel contentPane,enterTextPane,temp,temp2;
+	JLabel findFieldLabel,replaceFieldLabel;
+	JButton findNextButton,replaceButton,replaceAllButton,cancelButton;
 
 	public FindAndReplaceDialog(TextEditor editor,RSyntaxTextArea textArea,boolean isReplace){
 
 			this.textArea=textArea;
-			ComponentOrientation orientation = ComponentOrientation.getOrientation(getLocale());
+			orientation = ComponentOrientation.getOrientation(getLocale());
 			ifReplace=isReplace;
-			// Make a panel containing the "Find" edit box.
 			regexCB = new JCheckBox("Regex");
 			matchCaseCB = new JCheckBox("Match Case");
-			markAllCheckBox = new JCheckBox("Mark All");
-			wholeWordCheckBox=new JCheckBox("Whole Word");
-			JPanel enterTextPane = new JPanel(new SpringLayout());
-			searchField=new JTextField();
-			searchField.addFocusListener(new FindFocusAdapter());
-			searchField.addKeyListener(new FindKeyListener());
-			searchField.getDocument().addDocumentListener(new FindDocumentListener());
-			JPanel temp = new JPanel(new BorderLayout());
-			JLabel findFieldLabel=new JLabel("Find What");
-			temp.add(searchField);
-			temp.setBorder(BorderFactory.createLineBorder(Color.black));
-			if (orientation.isLeftToRight()) {
-				enterTextPane.add(findFieldLabel);
-				enterTextPane.add(temp);
-			}
-			else {
-				enterTextPane.add(temp);
-				enterTextPane.add(findFieldLabel);
-			}
-
+			markAllCB = new JCheckBox("Mark All");
+			wholeWordCB=new JCheckBox("Whole Word");
+			enterTextPane = new JPanel(new SpringLayout());
+			contentPane = new JPanel(new BorderLayout());
+			searchField =new JTextField();
+			createFieldLabel(searchField,findFieldLabel,"Find Next",temp);
 			if(ifReplace) {
 				replaceField=new JTextField();
-				replaceField.addFocusListener(new FindFocusAdapter());
-				replaceField.addKeyListener(new FindKeyListener());
-				replaceField.getDocument().addDocumentListener(new FindDocumentListener());
-				JPanel temp2 = new JPanel(new BorderLayout());
-				JLabel replaceFieldLabel=new JLabel("Replace with");
-				temp2.add(replaceField);
-				temp2.setBorder(BorderFactory.createLineBorder(Color.black));
-				if (orientation.isLeftToRight()) {
-					enterTextPane.add(replaceFieldLabel);
-					enterTextPane.add(temp2);
-				}
-				else {
-					enterTextPane.add(temp2);
-					enterTextPane.add(replaceFieldLabel);
-				}
+				createFieldLabel(replaceField,replaceFieldLabel,"Replace with",temp2);
 
 			}
 
-			makeSpringCompactGrid(enterTextPane,ifReplace ? 2:1, 2,	//rows, cols
-												0,0,		//initX, initY
-												6, 6);	//xPad, yPad
-
-			// Make a panel containing the inherited search direction radio
-			// buttons and the inherited search options.
+			makeSpringCompactGrid(enterTextPane,ifReplace ? 2:1, 2,	0,0,6, 6);
 			JPanel bottomPanel = new JPanel(new BorderLayout());
 			temp = new JPanel(new BorderLayout());
 			bottomPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
@@ -102,12 +74,12 @@ public class FindAndReplaceDialog extends JDialog implements ActionListener {
 			JPanel temp1 = new JPanel();
 			temp1.setLayout(new BoxLayout(temp1, BoxLayout.PAGE_AXIS));
 			temp1.add(matchCaseCB);
-			temp1.add(wholeWordCheckBox);
+			temp1.add(markAllCB);
 			searchConditionsPanel.add(temp1, BorderLayout.LINE_START);
 			temp1 = new JPanel();
 			temp1.setLayout(new BoxLayout(temp1, BoxLayout.PAGE_AXIS));
 			temp1.add(regexCB);
-			temp1.add(markAllCheckBox);
+			temp1.add(wholeWordCB);
 			searchConditionsPanel.add(temp1, BorderLayout.LINE_END);
 			temp.add(searchConditionsPanel, BorderLayout.LINE_START);
 			bottomPanel.add(temp, BorderLayout.LINE_START);
@@ -121,36 +93,21 @@ public class FindAndReplaceDialog extends JDialog implements ActionListener {
 			// Make a panel containing the action buttons.
 			JPanel buttonPanel = new JPanel();
 			buttonPanel.setLayout(new GridLayout(ifReplace ? 4:2,1, 5,5));
-			JButton findNextButton = new JButton("Find Next");
-			findNextButton.setActionCommand("Find Next");
+			findNextButton=new JButton("Find Next");
 			findNextButton.addActionListener(this);
 			buttonPanel.add(findNextButton);
 			if(ifReplace) {
-				JButton replaceButton = new JButton("Replace");
-				replaceButton.setActionCommand("Replace");
-				replaceButton.addActionListener(this);
-				buttonPanel.add(replaceButton);
-				JButton replaceAllButton = new JButton("Replace All");
-				replaceAllButton.setActionCommand("Replace All");
-				replaceAllButton.addActionListener(this);
-				buttonPanel.add(replaceAllButton);
+				createButton(buttonPanel,replaceButton,"Replace");
+				createButton(buttonPanel,replaceAllButton,"Replace All");
 			}
-			JButton cancelButton = new JButton("Cancel");
-			cancelButton.setActionCommand("Cancel");
-			cancelButton.addActionListener(this);
-			buttonPanel.add(cancelButton);
+			createButton(buttonPanel,cancelButton,"Cancel");
 			JPanel rightPanel = new JPanel();
 			rightPanel.setLayout(new BorderLayout());
 			rightPanel.add(buttonPanel, BorderLayout.NORTH);
 
 			// Put everything into a neat little package.
-			JPanel contentPane = new JPanel(new BorderLayout());
-			if (orientation.isLeftToRight()) {
-				contentPane.setBorder(BorderFactory.createEmptyBorder(5,0,0,5));
-			}
-			else {
-				contentPane.setBorder(BorderFactory.createEmptyBorder(5,5,0,0));
-			}
+
+
 			contentPane.add(leftPanel);
 			contentPane.add(rightPanel, BorderLayout.LINE_END);
 			temp = new JPanel(new BorderLayout());
@@ -163,60 +120,78 @@ public class FindAndReplaceDialog extends JDialog implements ActionListener {
 			applyComponentOrientation(orientation);
 	}
 
+	private void handleOrientation(JPanel textPane,JPanel panel,JLabel fieldLabel) {
+		if (orientation.isLeftToRight()) {
+				textPane.add(fieldLabel);
+				textPane.add(panel);
+				contentPane.setBorder(BorderFactory.createEmptyBorder(5,0,0,5));
+			}
+			else {
+				textPane.add(panel);
+				textPane.add(fieldLabel);
+				contentPane.setBorder(BorderFactory.createEmptyBorder(5,5,0,0));
+			}
+	}
+
+	private void createFieldLabel(JTextField field,JLabel label,String labelName,JPanel panel) {
+		panel = new JPanel(new BorderLayout());
+		label=new JLabel(labelName);
+		panel.add(field);
+		panel.setBorder(BorderFactory.createLineBorder(Color.black));
+		handleOrientation(enterTextPane,panel,label);
+	}
+
+	private void createButton(JPanel panel,JButton button,String name) {
+		button=new JButton(name);
+		button.addActionListener(this);
+		panel.add(button);
+	}
+
 	public void actionPerformed(ActionEvent e){
+
 		String action=e.getActionCommand();
-		if(action=="Find Next"){
-			String text = searchField.getText();
-         		if (text.length() == 0) {
-            			return;
-         		}
-         		boolean forward = true;
-         		boolean matchCase = matchCaseCB.isSelected();
-         		boolean wholeWord = wholeWordCheckBox.isSelected();
-			boolean markAll=markAllCheckBox.isSelected();
-         		boolean regex = regexCB.isSelected();
-        		boolean found = SearchEngine.find(textArea, text, forward,matchCase, wholeWord, regex);
-         		if (!found) {
-            			JOptionPane.showMessageDialog(this, "Fiji has finished searching the document");
-						textArea.setCaretPosition(0);
-         		}
-		}
-		if(action=="Replace"){
-			String text = searchField.getText();
-			if (text.length() == 0) {
-				return;
-			}
-			boolean forward = true;
-			boolean matchCase = matchCaseCB.isSelected();
-			boolean wholeWord = wholeWordCheckBox.isSelected();
-			boolean markAll=markAllCheckBox.isSelected();
-			boolean regex = regexCB.isSelected();
-			boolean replace =SearchEngine.replace(textArea, text,replaceField.getText(), forward,matchCase, wholeWord, regex);
-			if (!replace) {
-				JOptionPane.showMessageDialog(this, "Fiji has finished searching the document");
-				textArea.setCaretPosition(0);
-			}
-		}
-
-		if(action=="Replace All"){
-			String text = searchField.getText();
-			String replaceText=replaceField.getText();
-			if (text.length() == 0) {
-				return;
-			}
-			boolean forward = true;
-			boolean matchCase = matchCaseCB.isSelected();
-			boolean wholeWord = wholeWordCheckBox.isSelected();
-			boolean markAll=markAllCheckBox.isSelected();
-			boolean regex = regexCB.isSelected();
-			int replace =SearchEngine.replaceAll(textArea, text,replaceText,matchCase, wholeWord, regex);
-			JOptionPane.showMessageDialog(this, replace+" replacements made!");
-
-		}
-		if(action=="Cancel"){
+		if(action.equals("Cancel")){
 			this.dispose();
 		}
+		if(prepareForFindReplace()==0) return;
+		if(action.equals("Find Next")){
+			boolean found = SearchEngine.find(textArea,searchField.getText(), forward,matchCase, wholeWord, regex);
+         	messageAtEnd(found);
+		}
 
+		if(action.equals("Replace")){
+			boolean replace =SearchEngine.replace(textArea,searchField.getText(),replaceField.getText(), forward,matchCase, wholeWord, regex);
+			messageAtEnd(replace);
+		}
+
+		if(action.equals("Replace All")){
+			int replace =SearchEngine.replaceAll(textArea,searchField.getText(),replaceField.getText(),matchCase, wholeWord, regex);
+			JOptionPane.showMessageDialog(this, replace+" replacements made!");
+		}
+
+
+	}
+
+	private int prepareForFindReplace() {
+		if (searchField.getText().length() == 0) {
+			return 0;
+		}
+		setOptions();
+		return 1;
+	}
+
+	private void setOptions() {
+		matchCase = matchCaseCB.isSelected();
+		wholeWord = wholeWordCB.isSelected();
+		markAll=markAllCB.isSelected();
+		regex = regexCB.isSelected();
+	}
+
+	private void messageAtEnd(boolean value) {
+		if (!value) {
+			JOptionPane.showMessageDialog(this, "Fiji has finished searching the document");
+			textArea.setCaretPosition(0);
+		}
 	}
 
 	public boolean isReplace() {
@@ -235,7 +210,6 @@ public class FindAndReplaceDialog extends JDialog implements ActionListener {
 			return;
 		}
 
-		//Align all cells in each column and make them the same width.
 		Spring x = Spring.constant(initialX);
 		for (int c = 0; c < cols; c++) {
 			Spring width = Spring.constant(0);
@@ -252,8 +226,6 @@ public class FindAndReplaceDialog extends JDialog implements ActionListener {
 			}
 			x = Spring.sum(x, Spring.sum(width, Spring.constant(xPad)));
 		}
-
-		//Align all cells in each row and make them the same height.
 		Spring y = Spring.constant(initialY);
 		for (int r = 0; r < rows; r++) {
 			Spring height = Spring.constant(0);
@@ -269,8 +241,6 @@ public class FindAndReplaceDialog extends JDialog implements ActionListener {
 			}
 			y = Spring.sum(y, Spring.sum(height, Spring.constant(yPad)));
 		}
-
-		//Set the parent's size.
 		SpringLayout.Constraints pCons = layout.getConstraints(parent);
 		pCons.setConstraint(SpringLayout.SOUTH, y);
 		pCons.setConstraint(SpringLayout.EAST, x);
@@ -285,52 +255,6 @@ public class FindAndReplaceDialog extends JDialog implements ActionListener {
 		return layout.getConstraints(c);
 	}
 
-	private class FindDocumentListener implements DocumentListener {
-
-		public void insertUpdate(DocumentEvent e) {
-		}
-
-		public void removeUpdate(DocumentEvent e) {
-
-		}
-
-		public void changedUpdate(DocumentEvent e) {
-		}
-
-	}
-
-
-	/**
-	 * Listens for the text field gaining focus.  All it does is select all
-	 * text in the combo box's text area.
-	 */
-	private class FindFocusAdapter extends FocusAdapter {
-
-		public void focusGained(FocusEvent e) {
-
-		}
-
-	}
-
-
-	/**
-	 * Listens for key presses in the find dialog.
-	 */
-	private class FindKeyListener implements KeyListener {
-
-		// Listens for the user pressing a key down.
-		public void keyPressed(KeyEvent e) {
-		}
-
-		// Listens for a user releasing a key.
-		public void keyReleased(KeyEvent e) {
-		}
-
-		// Listens for a key being typed.
-		public void keyTyped(KeyEvent e) {
-		}
-
-	}
 
 }
 
