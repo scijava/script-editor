78bc280c43d91358f3428a930055b1807c4ebe1d
sumitdubey <sumitdubey54@gmail.com>
2009-08-08 15:15:38 +0530

    Changes in the sample test java file for debugging.

--- plugins/SamplePlugin.java
+++ plugins/SamplePlugin.java
@@ -3,19 +3,27 @@ import ij.*;
 import java.awt.Frame;
 
 public class SamplePlugin implements PlugIn {
-	static int foo;
-	public static void main(String args[]){
-		System.out.println("This is a plugin");
-	}
+	static int foo=0;
 	public void run(String path) {
+		int test=0;
+		test++;
+		boolean ass=false;
+		ass=true;
 		System.out.println("It is running");
-		foo=0;
-		foo++;
+		foo++; 
 		foo+=2;
-		foo--;
+		print();
 		foo+=5;
 		foo--;
 		foo-=2;
 	}
+	public void print() {
+		int test2=67;
+		test2+=3;
+		test2++;
+		test2-=2;
+		System.out.println("This is a test");
+	}
+
 }
 
