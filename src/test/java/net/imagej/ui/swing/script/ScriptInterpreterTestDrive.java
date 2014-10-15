5ee8a437a6e04181104c7b2c5d4703591e00effd
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-10-15 10:43:14 -0500

    Script interpreter test drive: add an auto-import example
    This makes it substantially easier to test the new feature.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/test/java/net/imagej/ui/swing/script/ScriptInterpreterTestDrive.java
+++ src/test/java/net/imagej/ui/swing/script/ScriptInterpreterTestDrive.java
@@ -33,10 +33,16 @@ package net.imagej.ui.swing.script;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import net.imagej.ui.swing.script.interpreter.InterpreterWindow;
 
 import org.scijava.Context;
+import org.scijava.app.SciJavaApp;
+import org.scijava.plugin.Plugin;
 
 /**
  * Interactive test for the script interpreter.
@@ -44,9 +50,11 @@ import org.scijava.Context;
  * @author Johannes Schindelin
  */
 public class ScriptInterpreterTestDrive {
+	private static InterpreterWindow interpreter;
+
 	public static void main(String[] args) throws Exception {
 		final Context context = new Context();
-		final InterpreterWindow interpreter = new InterpreterWindow(context);
+		interpreter = new InterpreterWindow(context);
 		interpreter.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosed(final WindowEvent e) {
@@ -54,5 +62,22 @@ public class ScriptInterpreterTestDrive {
 			}
 		});
 		interpreter.setVisible(true);
+	}
+
+	@Plugin(type = AutoImporter.class)
+	public static class XYZ implements AutoImporter {
+
+		@Override
+		public Map<String, List<String>> getDefaultImports() {
+			final String name = SciJavaApp.class.getName();
+			final int dot = name.lastIndexOf('.');
+			final String base = name.substring(dot + 1);
+			interpreter.print("Imported " + base + "; Try:\n\n\tprint(" + base + ".NAME);");
+
+			final Map<String, List<String>> map = new HashMap<String, List<String>>();
+			map.put(name.substring(0, dot), Arrays.asList(base));
+			return map;
+		}
+
 	}
 }
