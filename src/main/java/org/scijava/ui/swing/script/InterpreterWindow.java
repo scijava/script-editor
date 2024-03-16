aa76f7e1287862e7ac3864627666fec200689ae2
Curtis Rueden <ctrueden@wisc.edu>
2024-03-15 21:02:22 -0500

    Happy New Year 2024

--- src/main/java/org/scijava/ui/swing/script/InterpreterWindow.java
+++ src/main/java/org/scijava/ui/swing/script/InterpreterWindow.java
@@ -2,7 +2,7 @@
  * #%L
  * Script Editor and Interpreter for SciJava script languages.
  * %%
- * Copyright (C) 2009 - 2023 SciJava developers.
+ * Copyright (C) 2009 - 2024 SciJava developers.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
